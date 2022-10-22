98260d2964fdb080aff5cac5628b6cf2f139e65e
Ernesto E. Posada Pulido <ernestopos@hotmail.com>
2022-10-22 04:46:13 -0500

    Corrección error presentado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 import { PagesModule } from './pages/pages.module';
-import { UsuariosComponent } from './app/pages/usuarios/usuarios.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,   
-    NopagefoundComponent, UsuariosComponent      
+    NopagefoundComponent      
   ],
   imports: [
     BrowserModule,
