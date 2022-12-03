5246458a617f7a1f8937ef2d288dc4c33041a5a1
ernestopos <ernestopos@gmail.com>
2022-12-03 05:28:39 -0500

    API-REST

---
+++ src/app/services/api-rest-connect.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiRestConnectService {
+  private url: string = 'https://reqres.in/api/users?page=2';
+
+  constructor() {
+    const obs$ = new Observable((observer) => {
+      let i = 0;
+      const invervalo = setInterval(() => {
+        i++;
+        observer.next(i);
+        if (i == 20) {
+          clearInterval(invervalo);
+          observer.complete();
+        }
+      }, 1000);
+    });
+    obs$.subscribe((valor) => console.log('Subs:', valor));
+  }
+
+  getUsuario() {
+    const promesa = new Promise((resolve) => {
+      fetch(this.url)
+        .then((resp) => resp.json())
+        .then((body) => {
+          resolve(body.data);
+        });
+    });
+    return promesa;
+  }
+}
